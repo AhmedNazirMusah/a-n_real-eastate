d627bb1674c6af7c1047019e2353f072b4227544
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-10-28 12:22:29 +0000

    update

--- api/app.js
+++ api/app.js
@@ -1,16 +1,14 @@
-// import express from "express";
-const express = require('express')
-import postRoute from  "./routes/postRoute.js"
+import express from "express";
+import postRoute from  "./routes/postRoute.js";
+import authRoute from  "./routes/authRoute.js";
 
-// const postRoute = require('./routes/postRoute.js')
 
 const app = express();
 
-app.use("/api/test", (req,res) => {
-    res.send("it works")
-})  
+app.use(express.json())
 
-app.use("/api/", postRoute)
+app.use("/api/posts/", postRoute);
+app.use("/api/auth/", authRoute);
 
 app.listen(5000, () => {
     console.log("server is running");
