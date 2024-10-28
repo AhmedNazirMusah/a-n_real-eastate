8e9b1c52ba59351ce0d7ecb48e0ec05fb328d6bf
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-10-28 11:33:40 +0000

    update to app.js

--- api/app.js
+++ api/app.js
@@ -1,9 +1,17 @@
-import express from "express";
+// import express from "express";
+const express = require('express')
+import postRoute from  "./routes/postRoute.js"
 
-console.log("test 2")
+// const postRoute = require('./routes/postRoute.js')
 
 const app = express();
 
-app.listen(8800, () => {
-    console.log("server is running")
-})
+app.use("/api/test", (req,res) => {
+    res.send("it works")
+})  
+
+app.use("/api/", postRoute)
+
+app.listen(5000, () => {
+    console.log("server is running");
+});
